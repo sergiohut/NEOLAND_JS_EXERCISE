569862f741a32e87de7ef3b0532ba10f9e2ad523
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-10 21:40:17 +0100

    iteracción 3

---
+++ NEOLAND_BASIC_ALGORITHMS/iteración3.js
@@ -0,0 +1,22 @@
+//1.1 
+console.log(10*5);
+
+//1.2
+console.log(10/2);
+
+//1.3
+console.log(15%9);
+
+//1.4
+let p = 10;
+let j = 5;  
+let o = p + j;
+
+
+//me da la sensación que el enunciado esta mal y se trata de un operador y no un operador de asignación. 
+
+//1.5 
+let c = 10;
+let m = 5;
+let i = c * m;
+
