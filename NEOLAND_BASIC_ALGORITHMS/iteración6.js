28d79696a3b6133a9dfaeb7f9863be017b9d74bb
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-10 22:42:48 +0100

    iteración 6

---
+++ NEOLAND_BASIC_ALGORITHMS/iteración6.js
@@ -0,0 +1,19 @@
+//1.1
+for(let i=0; i<=9; i++){
+    console.log(i);
+}
+
+//1.2
+for(let i=0; i<=9;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+//1.3
+for(let i=0; i<=9; i++){
+    if(i<9){
+        console.log(`Intentando dormir🐑`)
+    } else {console.log(`Dormido!`) }
+}
+
