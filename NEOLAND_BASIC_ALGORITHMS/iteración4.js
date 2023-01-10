5cab5c907178860ff51a749472fe0689e3349f7e
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-10 22:08:02 +0100

    iteración 4

---
+++ NEOLAND_BASIC_ALGORITHMS/iteración4.js
@@ -0,0 +1,18 @@
+//1.1 
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+//1.2
+avengers.splice(0,1,"IRONMAN");
+console.log(avengers);
+
+//1.3
+console.log(avengers.length);
+
+//1.4
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty","Summer");
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
+
+
+
