e8b9d00bad3719d8f1c08633efac4ac4311bbffc
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-10 21:03:26 +0100

    cambio nombres

---
+++ NEOLAND_BASIC_ALGORITHMS/iteración2.js
@@ -0,0 +1,9 @@
+//1.1 
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+
+//1.2 
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`);
