2857204ab7d2a353e944458165cc543de3f25903
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-10 20:45:25 +0100

    iteración 1

---
+++ NEOLAND_BASIC_ALGORITHMS/Iteración 1.js
@@ -0,0 +1,14 @@
+//1.1 
+let myFavoriteHero = "Hulk";
+
+//1.2
+let x = 50;
+
+//1.3
+let h = 5;
+let y = 10; 
+
+// let h = 5; y = 10; también seria valido pero no es recomendable
+
+//1.4 
+let z = h+y; 
