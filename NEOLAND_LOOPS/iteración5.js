2e1a7714db92ea6ac90113d403553da2e524f839
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-11 22:18:37 +0100

    acabado

--- NEOLAND_LOOPS/iteración5.js
+++ NEOLAND_LOOPS/iteración5.js
@@ -1,6 +1,9 @@
 const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
 
 
-/*Tengo que hacer un for loop que me permita recorrer el array. 
-Al ir item por item tengo que buscar los ids que dice como condición y ejecutar que me elimine el item en caso afirmativo
-necesito mirar los methods de los objetos  */
+    let finalDestinationList = [];
+    for(place of placesToTravel){
+        if(place.id!== 11 || place.id !== 14){
+            finalDestinationList.push(place.id,place.name)
+        };
+    }
