824d66901c7b25e42a4ddae431a6b5f8abac5e36
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-12 16:07:18 +0100

    i5

---
+++ NEOLAND_BASIC_FUNCTIONS/iteración5.js
@@ -0,0 +1,16 @@
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param) {
+    for(let i = 0; i < param.length; i++){
+        let total = 0;
+        if(typeof param[i] === "number"){
+            total += param[i]}
+        else if (typeof param[i] === "string"){
+            total += param[i].length}
+        }
+        return total;
+    }
+
+console.log(averageWord(mixedElements))
+    
+
