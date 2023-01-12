23e2db773207678225d46469c717601568058c13
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-12 07:47:21 +0100

    revisión ejercicios después de repaso

--- NEOLAND_LOOPS/iteración2.js
+++ NEOLAND_LOOPS/iteración2.js
@@ -6,6 +6,39 @@ const alumns = [
 		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
 ] 
 
+for (let i = 0; i < alumns.length; i++){
+	if(alumns[i].T1 === true && alumns[i].T2 === true) {alumns[i].isApproved =true}
+	else if (alumns[i].T1 === true && alumns[i].T3 === true) {alumns[i].isApproved =true}
+	else if (alumns[i].T2 === true && alumns[i].T3 === true) {alumns[i].isApproved =true}
+  	else {alumns[i].isApproved =false}
+}
+
+
+console.log(alumns)
+
+
+
+
+
+
+/*Vale, si tengo que utilizar el loop forEach, ver que me imprime exactamente. Imprime todos los objetos del array 
+
+alumns.forEach((alumn)=>{console.log(alumn)})*/
+
+
+/*Conseguir que recorra una propiedad del array de objetos y compruebe su valor
+
+alumns.forEach((alumn)=>{if(alumn.T1 === true) {console.log(alumn)}})*/
+
+/* Comprobar todas las combinaciones posibles para que apruebe. 
+alumns.forEach((alumn)=>{
+	if(alumn.T1 === true && alumn.T2 === true) {console.log(alumn)}
+	else if (alumn.T1 === true && alumn.T3 === true) {console.log(alumn)}
+	else if (alumn.T2 === true && alumn.T3 === true) {console.log(alumn)}
+}) */
+
+/* Me habia liado por las pistas que dan y no sabia si habia que utilizar el for como dicen en el enunciado o el forEach como dicen en las pistas
+
 alumns.forEach((alumn)=>{
 	if(alumn.T1 === true && alumn.T2 === true) {alumn.isApproved =true}
 	else if (alumn.T1 === true && alumn.T3 === true) {alumn.isApproved =true}
@@ -13,44 +46,4 @@ alumns.forEach((alumn)=>{
   	else {{alumn.isApproved =false}}
 })
 
-console.log(alumns)
-
-
-
-/*Vale, tengo que utilizar el loop forEach, ver que me imprime exactamente. Imprime todos los objetos del array 
-const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
-] 
-alumns.forEach((alumn)=>{console.log(alumn)})
-
-
-/*Conseguir que recorra una propiedad del array de objetos y compruebe su valor
-const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
-] 
-
-alumns.forEach((alumn)=>{if(alumn.T1 === true) {console.log(alumn)}})*/
-
-/*
-
-const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
-] 
-
-alumns.forEach((alumn)=>{
-	if(alumn.T1 === true && alumn.T2 === true) {console.log(alumn)}
-	else if (alumn.T1 === true && alumn.T3 === true) {console.log(alumn)}
-	else if (alumn.T2 === true && alumn.T3 === true) {console.log(alumn)}
-}) */
+console.log(alumns)*/
