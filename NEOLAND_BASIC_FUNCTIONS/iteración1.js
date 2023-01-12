c6404ab07c4221549a5b632f153f49221de82be9
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-12 15:33:45 +0100

    iteración 3

--- NEOLAND_BASIC_FUNCTIONS/iteración1.js
+++ NEOLAND_BASIC_FUNCTIONS/iteración1.js
@@ -4,3 +4,5 @@ function sum(numberOne , numberTwo) {
 }
 
 console.log(sum(8,4));
+
+// como son solo dos valores no hace falta crear una variable donde almacenar, con un return es suficiente. Ahora bien, crear una variable lo haría más escalable
---
+++ NEOLAND_BASIC_FUNCTIONS/iteración3.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  for(let i = 0; i<param.length; i++){
+}
