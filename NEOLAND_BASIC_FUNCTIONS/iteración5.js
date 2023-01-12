c8503fce0c7f9f061285e589c69eced865b469fe
sergiohut <sergiosantiagocarmona@gmail.com>
2023-01-12 16:08:44 +0100

    i5 again

--- NEOLAND_BASIC_FUNCTIONS/iteración5.js
+++ NEOLAND_BASIC_FUNCTIONS/iteración5.js
@@ -1,8 +1,8 @@
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 
 function averageWord(param) {
+    let total = 0;
     for(let i = 0; i < param.length; i++){
-        let total = 0;
         if(typeof param[i] === "number"){
             total += param[i]}
         else if (typeof param[i] === "string"){
